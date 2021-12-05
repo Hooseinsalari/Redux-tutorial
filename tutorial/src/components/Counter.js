0587303237c964b508f3d2a523728617616f00e1
Hooseinsalari <hossein.salari002@gmail.com>
2021-12-05 22:32:20 +0330

    create store, reducer and action

---
+++ tutorial/src/components/Counter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Counter = () => {
+    return (
+        <div>
+            <h1>Counter - </h1>
+            <button>Up</button>
+        </div>
+    );
+};
+
+export default Counter;
